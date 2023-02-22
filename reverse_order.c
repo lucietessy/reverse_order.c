d2e19bb092e7d28f609560515c53eff94a512dba
lucietessy <123073199+lucietessy@users.noreply.github.com>
2023-02-22 15:45:27 +0300

    Add files via upload

---
+++ reverse_order.c
@@ -0,0 +1,11 @@
+//program to display first 50 natural numbers in reverse order
+#include <stdio.h>
+int main()
+{
+    int num;
+    printf("enter a positive number:");
+    scanf("%d",&num);
+    printf("first 50 natural numbers in reverse order");
+    return 0;
+}
+  
